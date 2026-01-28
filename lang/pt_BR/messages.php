7ceb7c19c430706158702b1e23b64d7a9f889b4a
Daniel Landim <danielc.landim@gmail.com>
2026-01-28 15:46:48 -0300

    add mesnages and validations translators

---
+++ lang/pt_BR/messages.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'product_removed' => 'Produto removido com sucesso.',
+    'product_updated' => 'Produto atualizado com sucesso.',
+    'product_created' => 'Produto criado com sucesso.',
+];
