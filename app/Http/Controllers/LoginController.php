1cdea09deefe8cb39d0fd12220f263f5aa26b5e9
Daniel Landim <danielc.landim@gmail.com>
2026-01-28 15:45:51 -0300

    add controller login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email'    => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Credenciais inválidas'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user'  => $user->only(['id', 'name', 'email']),
+        ]);
+    }
+}
